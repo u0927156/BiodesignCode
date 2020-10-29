335700468dba7d13673748e6b950ca37bbd82d71
Spencer Peterson <u0927156@utah.edu>
2020-10-29 14:42:15 -0600

    First Test Version
    Added code to handle receiving random inputs. Changed pins to correspond to final circuit layout.

--- ArduinoControllerTMS/ArduinoControllerTMS.ino
+++ ArduinoControllerTMS/ArduinoControllerTMS.ino
@@ -1,21 +1,28 @@
 
 int inputPin = 7; // Input will be connected to pin 7 
-int outPin = 13; // The voltage output will be to pin 13
+int outPin = 2; // The voltage output will be to pin 13
 int ledPin = 12; // Indicator LED will be connected to pin 12
-int voltIn = 0;
-int StimulationTime = 10; // seconds of stimulation time
-unsigned long timeOn; 
-unsigned long StimulationTimeInMillis;
-unsigned long shutOffTime;
+int voltIn = 0; // variable for reading voltage
+int StimulationTime = 3; // seconds of stimulation time
+unsigned long timeOn; // variable for time stimulation has been on
+unsigned long StimulationTimeInMillis; // Variable for how long to stimulate
+unsigned long shutOffTime; // variable for time to shut off
+
+// Variables to prevent random fluctuations from turning device on.
+int TimesNecessaryToStimulate = 10;
+int ReceivedOnSignals = 0;
 
 int wasOn = 0;
 int isOn = 0;
+unsigned long currTime; // variable for reading time
 
 void setup() {
+  // set up pins
   pinMode(inputPin, INPUT);
   pinMode(outPin, OUTPUT);
   pinMode(ledPin, OUTPUT);
-  
+
+  // serial for debugging
   Serial.begin(9600);
   // figure out the stimulation time in milliseconds. 
   StimulationTimeInMillis = StimulationTime * 1000; 
@@ -23,29 +30,42 @@ void setup() {
 
 void loop() {
   // Read the inputs
+  
   voltIn = digitalRead(inputPin);
-  //Serial.print(voltIn);
-  //Serial.print(" ");
-  //Serial.println(isOn);
-  // If the input is on, and the output isn't on already
-  if(voltIn == 1 && isOn == 0) 
-  {
-    isOn = 1;
-    timeOn = millis();
-    digitalWrite(outPin, HIGH);
-    digitalWrite(ledPin, HIGH);
 
-    // get the current time and calculate when to shut off.    
-    shutOffTime = timeOn + StimulationTimeInMillis;
-    Serial.println(timeOn);
-    Serial.println(shutOffTime);
-    
+
+  if(voltIn == LOW)
+  {
+    ReceivedOnSignals = 0;
   }
 
+  // if the input is on, and the output isn't on already
+  if(voltIn == 1 && isOn == 0) 
+  {
+    ReceivedOnSignals = ReceivedOnSignals + 1;
+
+    //Serial.println(ReceivedOnSignals);
+    if(ReceivedOnSignals >= TimesNecessaryToStimulate)
+    {
+      isOn = 1;
+      timeOn = millis();
+      digitalWrite(outPin, HIGH);
+      digitalWrite(ledPin, HIGH);
+  
+      // get the current time and calculate when to shut off.    
+      shutOffTime = timeOn + StimulationTimeInMillis;
+      //Serial.println(timeOn);
+     // Serial.println(shutOffTime);
+    }
+    
+  }
+ 
+
   // Turn of the input when the time has elapsed
-  unsigned long currTime = millis();
+  currTime = millis();
   if(isOn == 1 && currTime >= shutOffTime)
   {
+    ReceivedOnSignals = 0;
     Serial.println(currTime);
     digitalWrite(outPin, LOW);
     digitalWrite(ledPin, LOW); 
--- debugCode/debugCode.ino
+++ debugCode/debugCode.ino
@@ -1,7 +1,7 @@
 
 
-int ledPin = 12; // Indicator LED will be connected to pin 12
-
+int ledPin = 13; // Indicator LED will be connected to pin 12
+int ledPinOther = 12;
 void setup() {
   // put your setup code here, to run once:
 
@@ -11,7 +11,9 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   digitalWrite(ledPin,HIGH);
+  digitalWrite(ledPinOther,HIGH);
   delay(500);
   digitalWrite(ledPin,LOW);
+   digitalWrite(ledPinOther,LOW);
   delay(500);
 }
